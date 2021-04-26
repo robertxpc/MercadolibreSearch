912887460c9dbf0894b8a7b967ab8e0eed03bbc9
Roberto Parra <rparra@smartboxtv.com>
2021-04-25 21:55:43 -0400

    add test for Scene/MLItemDetail

--- MercadolibreSearchTests/Helpers/Network/MockMLServices+MLServiceProtocol.swift
+++ MercadolibreSearchTests/Helpers/Network/MockMLServices+MLServiceProtocol.swift
@@ -24,5 +24,25 @@ extension MockMLServices: MLServiceProtocol {
             return getError(with: error)
         }
     }
-    
+    func itemDetail(_ itemId: String) -> Observable<MLItem> {
+        switch mode {
+        case .done:
+            return getData(for: "done-item-detail")
+        case .empty:
+            return getData(for: "empty-search-list")
+        case .error(let error):
+            return getError(with: error)
+        }
+    }
+    func itemDescription(_ itemId: String) -> Observable<[MLItemDescription]> {
+        switch mode {
+        case .done:
+            return getData(for: "done-item-description")
+        case .empty:
+            return getData(for: "empty-item-description")
+        case .error(let error):
+            return getError(with: error)
+        }
+        
+    }
 }
---
+++ MercadolibreSearchTests/Scenes/ItemDetail/ItemDetailTests.swift
@@ -0,0 +1,130 @@
+//
+//  ItemDetailTests.swift
+//  MercadolibreSearchTests
+//
+//  Created by Roberto Parra Castillo on 25-04-21.
+//
+
+import XCTest
+import RxSwift
+import RxTest
+import RxBlocking
+
+@testable import MercadolibreSearch
+
+class ItemDetailTests: XCTestCase {
+    var services: MockMLServices!
+    var router: RouterTest!
+    var scheduler: TestScheduler!
+    
+    override func setUp() {
+        router = RouterTest()
+        services = MockMLServices()
+        scheduler = TestScheduler(initialClock: 0)
+        scheduler.start()
+    }
+    
+    override func tearDown() {
+        router = nil
+        services = nil
+        scheduler.stop()
+        scheduler = nil
+    }
+    func testItemDetailOnDone() {
+        services.mode = .done
+        
+        let item = MLItem(
+            id: "MLC-TEST",
+            title: nil,
+            price: 100,
+            originalPrice: nil,
+            currencyId: "CLP",
+            soldQuantity: 1,
+            thumbnail: nil,
+            installments: nil,
+            shipping: nil,
+            condition: .new,
+            acceptsMercadopago: true,
+            availableQuantity: 1,
+            pictures: nil)
+        
+        let disposeBag = DisposeBag()
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let viewModel = MLItemDetailViewModel(
+            router: router,
+            inputs: MLItemDetailViewModel.Inputs(
+                repository: services,
+                item: item))
+        
+        let observer: TestableObserver<Bool> = scheduler.createObserver(Bool.self)
+        
+        let correctLoadingExpected: [Recorded<Event<Bool>>] = [
+            Recorded.next(0, false),
+            Recorded.next(0, true),
+            Recorded.next(0, false)
+        ]
+        let errorsObserver: TestableObserver<Int> = scheduler.createObserver(Int.self)
+        
+        viewModel.loading.subscribe(observer).disposed(by: disposeBag)
+        viewModel.error.map({_ in 1}).subscribe(errorsObserver).disposed(by: disposeBag)
+        let resultObserver: TestableObserver<MLItem?> = scheduler.createObserver(MLItem?.self)
+
+        viewModel.item.subscribe(resultObserver).disposed(by: disposeBag)
+        
+        viewModel.start()
+        let results = resultObserver.events.last!.value.element!
+        XCTAssertEqual(correctLoadingExpected, observer.events)
+        XCTAssertNotNil(results)
+        
+        XCTAssertEqual([], errorsObserver.events)
+    }
+    func testItemDescriptionOnDone() {
+        services.mode = .done
+        
+        let item = MLItem(
+            id: "MLC-TEST",
+            title: nil,
+            price: 100,
+            originalPrice: nil,
+            currencyId: "CLP",
+            soldQuantity: 1,
+            thumbnail: nil,
+            installments: nil,
+            shipping: nil,
+            condition: .new,
+            acceptsMercadopago: true,
+            availableQuantity: 1,
+            pictures: nil
+            )
+        
+        let disposeBag = DisposeBag()
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let viewModel = MLItemDetailViewModel(
+            router: router,
+            inputs: MLItemDetailViewModel.Inputs(
+                repository: services,
+                item: item))
+        
+        let observer: TestableObserver<Bool> = scheduler.createObserver(Bool.self)
+        
+        let correctLoadingExpected: [Recorded<Event<Bool>>] = [
+            Recorded.next(0, false),
+        ]
+        let errorsObserver: TestableObserver<Int> = scheduler.createObserver(Int.self)
+        
+        viewModel.loading.subscribe(observer).disposed(by: disposeBag)
+        viewModel.error.map({_ in 1}).subscribe(errorsObserver).disposed(by: disposeBag)
+        let resultObserver: TestableObserver<[MLItemDescription]> = scheduler.createObserver([MLItemDescription].self)
+
+        viewModel.itemDescription.subscribe(resultObserver).disposed(by: disposeBag)
+        
+        viewModel.getDescription()
+        let results = resultObserver.events.last!.value.element!
+        XCTAssertEqual(correctLoadingExpected, observer.events)
+        XCTAssertGreaterThan(results.count, 0)
+        XCTAssertEqual([], errorsObserver.events)
+    }
+    
+}
